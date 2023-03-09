10cb9bd4cb5d7a4849f013524f1e2ba8e4f4b171
monia-0708 <laskowskamonia90@gmail.com>
2023-03-09 10:12:29 +0100

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Header from '.components/Header';
+import Header from './components/Header';
 
 import AddPrice from "./components/AddPrice";
 import Prices from "./components/Prices";
 
 import Signin from "./views/Signin";
-import Signup from "./views/Singup";
+import Signup from "./views/Signup";
+
 
 
 function App() {
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,13 @@
+import { Button } from 'primereact/button';
+import 'primeicons/primeicons.css';
+
+
 function Header() {
-    return <h1>Header</h1>;
+    return <h1>
+        <div>FuelUp</div>
+        <Button icon="pi pi-user" rounded text severity="info" aria-label="User" />
+        
+    </h1>;
 }
 
 export default Header;
