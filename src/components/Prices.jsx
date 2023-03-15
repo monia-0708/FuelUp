17f5ff452f625a18fda82524e5477e400208f248
monia-0708 <laskowskamonia90@gmail.com>
2023-03-15 23:51:45 +0100

    zmiana wydokow w komponencie Prices

--- src/components/Prices.jsx
+++ src/components/Prices.jsx
@@ -2,32 +2,47 @@ import { Button } from "primereact/button";
 import Array from "./Array";
 import Map from "./Map";
 import React, { useState } from "react";
-import Search from './Search'; 
+import Search from "./Search";
+
 function Prices() {
   const [set, setSet] = useState(1);
+  const [showSearch, setShowSearch] = useState(false);
 
   const handleSetSearch = () => {
     setSet(1);
-  }
+    setShowSearch(true);
+  };
 
   const handleSetMap = () => {
     setSet(2);
-  }
+    setShowSearch(false);
+  };
 
   return (
     <>
-      <div className="button-card" >
+      <div className="button-card">
         <div className="card flex justify-content-center">
-          <span className="p-buttonset" >
-            <Button style={{backgroundColor: "#800080"}} onClick={handleSetSearch} label="Filtruj" icon="pi pi-list" />
-            
-            
-            <Button style={{backgroundColor: "#800080"}} onClick={handleSetMap} label="Mapa" icon="pi pi-map" />
+          <span className="p-buttonset">
+            <Button
+              className="button-filtruj"
+              style={{ backgroundColor: "#800080" }}
+              onClick={handleSetSearch}
+              label="Filtruj"
+              icon="pi pi-list"
+            />
+
+            <Button
+              className="button-mapa"
+              style={{ backgroundColor: "#800080" }}
+              onClick={handleSetMap}
+              label="Mapa"
+              icon="pi pi-map"
+            />
           </span>
-          {set === 2 && <Map />} {/* warunek, aby wyświetlić Map tylko dla set=2 */}
-          {set === 1 && <Search />} {/* warunek, aby wyświetlić Search tylko dla set=1 */}
-          {set === 1 && <Array />}
-           {/* warunek, aby wyświetlić Array tylko dla set=1 */}
+
+          {set === 1 && showSearch ? <Search /> : null}
+          {set === 2 && <Map />}
+          {set === 1 && !showSearch ? <Array /> : null}
         </div>
       </div>
     </>
@@ -35,4 +50,3 @@ function Prices() {
 }
 
 export default Prices;
-
